3383c0f7864897c3a64c0e9c706ff7a368c6d616
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-16 12:58:47 -0300

    create a header component

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import Header from '~/components/Header';
 // import { Container } from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Repositorios</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
